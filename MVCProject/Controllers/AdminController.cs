53d0e3e30d5c574d4ba22d909d52264f71ddeb01
Elizabeth H <48732974+ElizabethHC@users.noreply.github.com>
2019-10-04 16:38:21 -0700

    Car Insurance

---
+++ MVCProject/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using MVCProject.Models;
+using MVCProject.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (CarInsuranceEntities db = new CarInsuranceEntities())
+            {
+                var applying = db.Applicants;
+                var applyVms = new List<ApplyVM>();
+                foreach (var apply in applying)
+                {
+                    var applyVm = new ApplyVM();
+                    applyVm.FirstName = apply.FirstName;
+                    applyVm.LastName = apply.LastName;
+                    applyVm.EmailAddress = apply.EmailAddress;
+                    applyVm.Quote = apply.Quote;
+                    applyVms.Add(applyVm);
+                }
+
+                return View(applyVms);
+            }
+        }
+    }
+}
---
+++ MVCProject/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using MVCProject.Models;
+using MVCProject.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Apply(string firstName, string lastName, string emailAddress, 
+            string birthDate, string carYear, string carMake, string carModel, string DUI,
+            string tickets, string coverageType)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress) ||
+                string.IsNullOrEmpty(birthDate) || string.IsNullOrEmpty(carYear) || string.IsNullOrEmpty(carMake) ||
+                string.IsNullOrEmpty(carModel) || string.IsNullOrEmpty(DUI) || string.IsNullOrEmpty(tickets) ||
+                string.IsNullOrEmpty(coverageType))
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                using (CarInsuranceEntities db = new CarInsuranceEntities())
+                {
+                    var apply = new Applicant();
+                    apply.FirstName = firstName;
+                    apply.LastName = lastName;
+                    apply.EmailAddress = emailAddress;
+                    apply.BirthDate = birthDate;
+                    apply.CarYear = carYear;
+                    apply.CarMake = carMake;
+                    apply.CarModel = carModel;
+                    apply.DUI = DUI;
+                    apply.Tickets = tickets;
+                    apply.CoverageType = coverageType;
+
+                    db.Applicants.Add(apply);
+                    db.SaveChanges();
+
+
+                }
+
+                    return View("Success");
+            }
+
+        }
+    }
+}
